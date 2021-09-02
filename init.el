78aa5a4f8d5e192dd9f9733abd7d0ba629f1c55f
Antonio Llamas <antoniollamasln@gmail.com>
2021-09-02 18:39:38 +0200

    Clean UI

---
+++ init.el
@@ -0,0 +1,13 @@
+;; Disable startup message
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1) ;; Disable visible scroll bar
+(tool-bar-mode -1) ;; Disable the toolbar
+(tooltip-mode -1) ;; Disable tooltip
+(set-fringe-mode 10) ;; Give me padding left and right
+
+(menu-bar-mode -1) ;; Disable de bar menu
+
+;; Set up the visible bell
+(setq visible-bell t)
+
